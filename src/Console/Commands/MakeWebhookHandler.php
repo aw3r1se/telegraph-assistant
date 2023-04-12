2ff29a26d3f023fac3c28990afe352a5fa787b78
koch <koch@echo-company.ru>
2023-04-12 19:58:13 +0500

    handler

--- src/Console/Commands/MakeWebhookHandler.php
+++ src/Console/Commands/MakeWebhookHandler.php
@@ -86,7 +86,7 @@ class MakeWebhookHandler extends Command
         $basename = class_basename($new);
         $contents = preg_replace("#StubParent#ui", $basename, $contents);
 
-        return preg_replace('#\n{2,}#ui', PHP_EOL, $contents);
+        return preg_replace('#\n{2,}#ui', PHP_EOL . PHP_EOL, $contents);
     }
 
     /**
